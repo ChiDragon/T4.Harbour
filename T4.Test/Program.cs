9d9d2065ecab403f464cf4519f569fe41775b941
Harbour <864353016@qq.com>
2018-02-07 15:59:23 +0800

    加入测试

---
+++ T4.Test/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace T4.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MySqlDbHelper mysql = new MySqlDbHelper();
+            List<DbTable> dbTable = mysql.GetDbTables();
+        }
+    }
+}
